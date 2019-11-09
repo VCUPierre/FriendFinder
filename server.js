3f63ea824625ff6572994b4bbd21b02d6645c77f
Pierre R <randalltp@mymail.vcu.edu>
2019-11-09 16:06:36 -0500

    built html routes

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+//require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+
+//starts app on port 8080
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
